14ce4a5ce4225004cf92bb3ad0d78f01d9cdeb30
Mauro Aguilar <mauro.aguilar@davita.com>
2022-07-05 20:33:14 -0500

    troubleshoot asset loading problem in github pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // Temporary solution for github pages!
+  base: 'product-preview-card-frontend-mentor',
   build: {
     outDir: '../',
   },
